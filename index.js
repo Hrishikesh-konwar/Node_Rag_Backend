08feacefb06de11856e74d549a26c178f62c18c6
Hrishikesh Konwar <53933663+Hrishikesh-konwar@users.noreply.github.com>
2025-05-09 19:18:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import { updateChat, chat } from './controller.js';
+import { createUser, getUsers } from './user.js';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 8080;
 
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(console.log('MongoDb connected'))
+  .catch((err) => console.log('Mongo Error', err));
+
+
 // Routes
+app.post('/createUser', createUser);
+app.get('/getUsers', getUsers);
+
 app.use('/updateChat', updateChat);
 app.use('/chat', chat);
 
