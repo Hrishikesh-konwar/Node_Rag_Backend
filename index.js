0990a8334533b04e1b1390b864586e4a60201ba1
Hrishikesh Konwar <53933663+Hrishikesh-konwar@users.noreply.github.com>
2025-05-09 20:32:28 +0530

    update

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+import https from 'https';
+
+https.get('https://api.ipify.org', (res) => {
+  res.on('data', (ip) => {
+    console.log(`Render server IP is: ${ip}`);
+  });
+});
+
+
 const app = express();
 app.use(express.json());
 
